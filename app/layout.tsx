463d6afb3cb83e3b20ba48dec86c627fe8283f60
rakesh <rakeshcodes2005@gmail.com>
2025-08-02 02:45:16 +0530

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DotPatternWithGlowEffectDemo } from "@/components/magicui/background";
-import { ReactElement, ReactNode } from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,18 +18,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Hi i'm Rakesh",
+  title: "Hi I'm Rakesh",
   description: "Personal Portfolio website",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">  
-      <body className="mx-auto px-4 min-w-5xl">
-          <div className={`${geistSans.variable} ${geistMono.variable}`}>
-            {children}
-            <Analytics />
-          </div>
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} mx-auto px-4 max-w-screen-xl`}>
+        {children}
+        <Analytics />
       </body>
     </html>
   );
